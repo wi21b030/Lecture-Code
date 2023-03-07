75019ffea45ca8d38aa9b544c9902cab04224096
wi21b030 <wi21b030@technikum-wien.at>
2023-03-07 15:58:43 +0100

    lecture code of class 2

--- Basic-2/main.js
+++ Basic-2/main.js
@@ -1,5 +1,26 @@
+
 function changeHeadline(){
     let newValue = document.getElementById("headline-text").value
     console.log("New value will be: " + newValue)
     document.getElementById("headline").innerText = newValue
 }
+
+function changeHeadlineInputValue(newHeadline){
+    document.getElementById("headline").innerText = newHeadline
+}
+
+function testCode(){
+    let eingabe = prompt("Gib etwas ein: ")
+    document.write(eingabe)
+}
+
+function addItemEvent(){
+   let liElementsList = document.getElementsByTagName("li")
+    for (let i = 0; i < liElementsList.length; i++) {
+        let newValue = liElementsList[i].innerText
+        //liElementsList[i].addEventListener("click", function (){changeHeadlineInputValue(newValue)})
+        liElementsList[i].addEventListener("mouseover", function (){changeHeadlineInputValue(newValue)})
+    }
+}
+
+addItemEvent()
