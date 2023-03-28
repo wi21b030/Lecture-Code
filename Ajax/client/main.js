a7816d1bd2803f56f68fcca9dbf9f22d5edabdcf
wi21b030 <wi21b030@technikum-wien.at>
2023-03-28 17:27:15 +0200

    formatting

--- Ajax/client/main.js
+++ Ajax/client/main.js
@@ -1,7 +1,7 @@
 let url = "../api?method=read"
 
 function addPersonEntry(person){
-    let $tr = $('tr')
+    let $tr = $('<tr>')
     $tr.append('<td>' + person.firstname + '</td>')
     $tr.append('<td>' + person.lastname + '</td>')
     $('#persons-table').find('tbody').append($tr)
